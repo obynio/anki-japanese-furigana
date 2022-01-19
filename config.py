4fc618c0745bfd5bf98d399b207e3f03158a8d96
Yohann Leon <yohann@leon.re>
2022-01-19 22:55:11 +0100

    Use anki default config engine

--- config.py
+++ config.py
@@ -1,32 +1,44 @@
-import json
-import os
+# -*- coding: utf-8 -*-
+
+# This file is part of Japanese Furigana <https://github.com/obynio/anki-japanese-furigana>.
+#
+# Japanese Furigana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Japanese Furigana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Japanese Furigana.  If not, see <http://www.gnu.org/licenses/>.
 
 from aqt import mw
 
-path = os.path.join(os.path.dirname(__file__), "config.json")
-
-def saveconfig(func):
+def saveMe(func):
     def wrapper(self, *args):
         func(self, args)
+        mw.addonManager.writeConfig(__name__, self.data)
 
-        with open(path, 'w') as outfile:
-            json.dump(self.data, outfile, indent=4)
     return wrapper
 
 class Config:
-    with open(path) as f:
-         data = json.load(f)
+    data = mw.addonManager.getConfig(__name__)
 
     def getUseRubyTags(self):
         return self.data['useRubyTags']
 
-    @saveconfig
+    @saveMe
     def setUseRubyTags(self, isEnabled):
         self.data['useRubyTags'] = isEnabled[0]
 
     def getIgnoreNumbers(self):
         return self.data['ignoreNumbers']
 
-    @saveconfig
+    @saveMe
     def setIgnoreNumbers(self, isEnabled):
         self.data['ignoreNumbers'] = isEnabled[0]
+
+
